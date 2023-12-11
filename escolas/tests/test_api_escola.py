a5572242b7e3310f52ba345edd52b8f0a9ff6c78
Matheus Cardoso Teixeira <matheus.teixera@dcomp.ufs.br>
2023-12-11 14:30:59 -0300

    Corrigindo os testes

--- escolas/tests/test_api_escola.py
+++ escolas/tests/test_api_escola.py
@@ -6,24 +6,24 @@ from escolas.tests.test_base_escola import EscolaTestBase
 
 class EscolaAPITestCase(EscolaTestBase):
     def test_list_escolas(self):
-        url = reverse('escolas:escolas-api-list')
+        url = reverse('escolas:escola-api-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_retrieve_escola(self):
-        url = reverse('escolas:escolas-api-detail', args=[self.escola.id])
+        url = reverse('escolas:escola-api-detail', args=[self.escola.id])
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_if_retrive_escola_returns_404_if_escola_not_found(self):
-        url = reverse('escolas:escolas-api-detail', args=[10])
+        url = reverse('escolas:escola-api-detail', args=[10])
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     # Adicione mais testes conforme necessário para as operações CRUD
 
     def test_create_escola(self):
-        url = reverse('escolas:escolas-api-list')
+        url = reverse('escolas:escola-api-list')
         data = {'cnpj': '00000000000001', 'nome': 'Nova Escola', 'endereco': 'Novo Endereço'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -31,19 +31,19 @@ class EscolaAPITestCase(EscolaTestBase):
     def test_if_non_superusers_receive_error_403_using_create_escola(self):
         self.user.is_superuser = False
         self.user.save()
-        url = reverse('escolas:escolas-api-list')
+        url = reverse('escolas:escola-api-list')
         data = {'cnpj': '00000000000001', 'nome': 'Nova Escola', 'endereco': 'Novo Endereço'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_if_incorrect_data_receive_error_400_using_create_escola(self):
-        url = reverse('escolas:escolas-api-list')
+        url = reverse('escolas:escola-api-list')
         data = {'nome': 'Nova Escola', 'endereco': 'Novo Endereço'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_update_escola(self):
-        url = reverse('escolas:escolas-api-detail', args=[self.escola.id])
+        url = reverse('escolas:escola-api-detail', args=[self.escola.id])
         data = {'nome': 'Escola Atualizada', 'endereco': 'Endereço Atualizado'}
         response = self.client.patch(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -51,30 +51,30 @@ class EscolaAPITestCase(EscolaTestBase):
     def test_if_non_superusers_receive_error_403_using_update_escola(self):
         self.user.is_superuser = False
         self.user.save()
-        url = reverse('escolas:escolas-api-detail', args=[self.escola.id])
+        url = reverse('escolas:escola-api-detail', args=[self.escola.id])
         data = {'nome': 'Escola Atualizada', 'endereco': 'Endereço Atualizado'}
         response = self.client.patch(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_if_update_escola_returns_404_if_not_found_escola(self):
-        url = reverse('escolas:escolas-api-detail', args=[10])
+        url = reverse('escolas:escola-api-detail', args=[10])
         data = {'nome': 'Escola Atualizada', 'endereco': 'Endereço Atualizado'}
         response = self.client.patch(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_delete_escola(self):
-        url = reverse('escolas:escolas-api-detail', args=[self.escola.id])
+        url = reverse('escolas:escola-api-detail', args=[self.escola.id])
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_if_non_superusers_receive_error_403_using_delete_urls(self):
         self.user.is_superuser = False
         self.user.save()
-        url = reverse('escolas:escolas-api-detail', args=[self.escola.id])
+        url = reverse('escolas:escola-api-detail', args=[self.escola.id])
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_if_delete_escola_returns_404_if_not_found_escola (self):
-        url = reverse('escolas:escolas-api-detail', args=[10])
+        url = reverse('escolas:escola-api-detail', args=[10])
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
