67c0fb6700457cc21eedc4719c028ed2f8926564
Matheus Cardoso Teixeira <matheus.teixera@dcomp.ufs.br>
2024-04-18 18:10:49 -0300

    ajustando os serializers

--- escolas/serializers/escola_sem_sala_serializer.py
+++ escolas/serializers/escola_sem_sala_serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
 from escolas.models import Escola
+from escolas.serializers.email_escola_serializer import EmailEscolaSerializer
+from escolas.serializers.telefone_escola_serializer import TelefoneEscolaSerializer
 
 
 class EscolaSemSalaSerializer(serializers.ModelSerializer):
@@ -17,4 +19,16 @@ class EscolaSemSalaSerializer(serializers.ModelSerializer):
             'criado_em',
             'atualizado_em',
             'imagem',
+            'escola_telefones',
+            'escola_emails',
         ]
+    objetos_telefones = TelefoneEscolaSerializer(
+        many=True,
+        source='escola_telefones',
+        read_only=True,
+    )
+    objetos_emails = EmailEscolaSerializer(
+        many=True,
+        source='escola_emails',
+        read_only=True,
+    )
