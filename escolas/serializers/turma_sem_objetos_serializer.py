bec2703b557fab14d0259c7be387d5b0b5e590fb
Matheus Cardoso Teixeira <matheus.teixera@dcomp.ufs.br>
2024-04-21 22:00:53 -0300

    ajustando os serializers

--- escolas/serializers/turma_sem_objetos_serializer.py
+++ escolas/serializers/turma_sem_objetos_serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from escolas.models import Turma
-from escolas.serializers import SalaSemTurmaSerializer
+from escolas.serializers.sala_sem_turma_serializer import SalaSemTurmaSerializer
 
 
 class TurmaSemObjetosSerializer(serializers.ModelSerializer):
